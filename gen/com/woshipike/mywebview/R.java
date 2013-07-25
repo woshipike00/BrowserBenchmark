8a1d04d47162bd1cc74a2864e3e6a7cb05a07493
woshipike00 <pike@pike-OptiPlex-780.(none)>
2013-07-25 11:09:31 +0800

    add start function

--- gen/com/woshipike/mywebview/R.java
+++ gen/com/woshipike/mywebview/R.java
@@ -24,8 +24,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080004;
         public static final int button1=0x7f080001;
+        public static final int button2=0x7f080003;
         public static final int editText1=0x7f080000;
         public static final int webView1=0x7f080002;
     }
@@ -36,10 +37,12 @@ public final class R {
         public static final int customized_webview=0x7f070000;
     }
     public static final class string {
+        public static final int GO=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int go=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int start=0x7f050005;
     }
     public static final class style {
         /** 
---
+++ src/com/fujitsu/fnsk/Widget/ScrollWebview.java
@@ -0,0 +1,77 @@
+package com.fujitsu.fnsk.Widget;
+
+import java.util.Timer;
+
+import android.content.Context;
+import android.support.v4.util.TimeUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.animation.LinearInterpolator;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Scroller;
+
+public class ScrollWebview extends WebView{
+
+	
+	private Context context;
+	private Scroller scroller;
+	private boolean tag=false;
+	private String oldurl;
+
+
+	public ScrollWebview(Context context) {
+		super(context);
+		this.context=context;
+		scroller=new Scroller(context);
+		oldurl="";
+
+	}
+	
+	public ScrollWebview(Context context,AttributeSet set){
+
+		super(context, set);
+		this.context=context;
+		scroller=new Scroller(context,new LinearInterpolator());
+		oldurl="";
+
+
+	}
+	
+	
+
+
+
+	@Override
+	public void computeScroll() {
+		if(tag && scroller.computeScrollOffset()) {
+			int x=scroller.getCurrX();
+	    	int y=scroller.getCurrY();
+	    	scrollTo(x, y);
+	    	postInvalidate();
+	
+		}
+		super.computeScroll();
+	}
+
+
+	
+	
+	public Scroller getScroller(){
+		return scroller;
+	}
+	
+	public void settag(){
+		tag=true;
+	}
+	
+	public void setoldurl(String url){
+		oldurl=url;
+	}
+	
+	public String getoldurl(){
+		return oldurl;
+	}
+	
+
+}
